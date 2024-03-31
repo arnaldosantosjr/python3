28f61b75898205436431d1a660fb0bf47cdfa6d6
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-03-31 11:22:55 -0300

    intAndFloat
    Aqui eu exercitei os tipos primitivos e a função type

---
+++ pytonudemy/auula004.py
@@ -0,0 +1,23 @@
+# Tipos int e float
+# int -> Número inteiro
+#Tipo int representa qualquer número positivo ou negativo.
+#int sem sinal é considerado positivo.
+
+print(11) #int
+print(-11) #int
+print(0) 
+
+#float -> Número com ponto flutuante 
+# O tipo float representa qualquer número positivo ou negativo.
+#float sem sinal é considerado positivo.
+
+print(1.1) #float
+print(0.0) #float
+print(-1.2) #float
+
+
+# A função type mostra o tipo que o Python  inferiu ao valor
+
+print(type('Arnaldo'))
+print(type(1))
+print(type(1.3),type(-1.1), type(0.0))
