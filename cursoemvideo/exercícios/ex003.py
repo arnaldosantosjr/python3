0df749dce34323a505d12edb91a4b9e1ddb0a343
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-03-31 12:38:47 -0300

    Correction
    Corrigindo o exercício 004

---
+++ cursoemvideo/exercícios/ex003.py
@@ -0,0 +1,6 @@
+''''Este programa pede para o usuário digitar dois números e depois exibirá a soma entre eles
+O usúario deverá inserir números reais'''
+n1= float(input('Digite o primeiro número:\n '))
+n2 = float(input('Digite o segundo número: \n'))
+soma = n1+n2
+print(f'A soma entre {n1} e {n2} é {soma}')
--- cursoemvideo/exercícios/ex004.py
+++ cursoemvideo/exercícios/ex004.py
@@ -1,6 +1,20 @@
-''''Este programa pede para o usuário digitar dois números e depois exibirá a soma entre eles
-O usúario deverá inserir números reais'''
-n1= float(input('Digite o primeiro número:\n '))
-n2 = float(input('Digite o segundo número: \n'))
-soma = n1+n2
-print(f'A soma entre {n1} e {n2} é {soma}')
+''''
+Este programa pedirá par o usuário digitar algo.
+Depois ele irá exibir o tipo primitivo doque foi digitado
+Por último ele exibirá todas as informações possíveis sobre o que foi digitado
+'''
+x = input('Digite algo: \n')
+print('Qual a classe do valor?',type(x))
+print('É numérico? ',x.isnumeric())
+print('É alfanumérico? ',x.isalnum())
+print('É Alfabético?',x.isalpha())
+print('Tem caractére imprimível? ',x.isprintable())
+print('Está em maiúsculo?',x.isupper())
+print('Está em minúsculo?',x.islower())
+print('Tem espaço? ',x.isspace())
+print('Está capitalizado? ', x.istitle()) #Minúsculo e maiúsculo
+
+
+
+
+
