a337f8c6857cefa58a107c156d55e590d4e9e329
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-03-31 11:41:00 -0300

    tipobool
    Aqui eu exercitei o topo boolean(Booleano) - > Verdadeiro ou falso

---
+++ pytonudemy/aula005.py
@@ -0,0 +1,13 @@
+#Tipo de dao bool (Boolean)
+#Ao questionar algo em um programa só exitem duas respostas possíveis:
+# sim(True) ou não(False)
+#Exitem vários operadores para "questionar".
+#Dentre os operadore o ==, que é um operador lógico que quetiona se o valro é igualo ao outro.
+
+print(10 == 10) # sim => True (verdadeiro)
+print(type(10 == 10)) 
+print(10 == 11) #Não => False (falso)
+print(type(10 == 11)) 
+print(type(True)) 
+print(type(False)) 
+
