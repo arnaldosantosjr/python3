8c599d9782a64bf43589db73eb587a51094783e1
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-04-07 20:25:43 -0300

    variáveis
    Aqui aprendi e fiz um exercício sobre variáveis

--- pytonudemy/aula007.py
+++ pytonudemy/aula007.py
@@ -4,6 +4,14 @@
 # O sinal de = é operador de atribuição. Ele é usadao para atribuir um valor a um nome (variável).
 # Uso: nome_variável = expressão
 
-nome_completo = 'Aranldo Santos Leão'
+'''nome_completo = 'Aranldo Santos Leão'
 soma_dois_mais_dois = 2 + 2
-print(nome_completo, soma_dois_mais_dois) # Arnaldo Santos Leão
+int_um = bool('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois) # Arnaldo Santos Leão'''
+
+nome = 'Arnaldo'
+idade = 25
+maior_de_idade = idade >= 18
+print(f'Nome:{nome}, idade {idade}')
+print(f'É amior? {maior_de_idade}')
---
+++ pytonudemy/aula008.py
@@ -0,0 +1,13 @@
+nome = 'Arnaldo'
+sobrenome = 'Santos'
+idade = 18
+ano_nascimento = 2024 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.81
+
+print(f'Nome: {nome}')
+print(f'Sobrenome: {sobrenome}')
+print(f'Idade: {idade}')
+print(f'Ano de nascimento: {ano_nascimento}')
+print(f'É maio de idade? {maior_de_idade}')
+print(f'Altura em metros: {altura_metros}')
