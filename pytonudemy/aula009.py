69bbd772e67c700e9be1eddbf4e91a628f53919f
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-04-08 18:26:18 -0300

    aula009.py
    Aula sobre opera dores aritiméticos

---
+++ pytonudemy/aula009.py
@@ -0,0 +1,27 @@
+#Operação aritméticas
+adicao  = 10 + 10
+print(f'A adição é {adicao}')
+
+subtracao = 10 - 5
+print(f'A subtração é {subtracao}')
+
+multiplicacao = 10 * 10
+print(f'A Multiplicação é {multiplicacao}')
+
+divisao = 10 / 2.2
+print(f'A divisão é {divisao}')
+
+divisao_inteira = 10 // 2.2
+print(f'A divisão inteira é {divisao_inteira}')
+
+exponenciacao = 2 ** 10
+print(f'A exponeciação é {exponenciacao}')
+
+modulo = 55 % 2 #Resto da divisão
+print(f'O módulo ou resto da divisão é {modulo}')
+
+print(10 % 8 == 0)
+print(16 % 8 == 0)
+print(10 % 2 == 0)
+print(15 % 2 == 0)
+print(16 % 2 == 0)
