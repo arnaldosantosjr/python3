94b33d5f1451e3050da5ae8f28d4d2e8c26b4337
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-04-09 16:50:53 -0300

    ConvertendoTipos
    convertendo tipos de dados

---
+++ DioMe/Aula02/03convertendo_tipos.py
@@ -0,0 +1,27 @@
+'''preco = 10.50
+idade  = 23
+
+print(str(preco))
+print(str(idade))#Convertendo para string
+print(float(preco))#convertendo para float
+
+#Segunda forma de converter 
+#assim está concatenado
+texto = f"idade {idade} preco {preco}"
+print(texto)
+'''
+
+print(int(1.9)) #Convertendo float para inteiro
+print(int('10')) #str para int
+print(float('10.10')) #str para float
+print(float(100)) #int para float
+
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+print (str(10.10)) #float para str
+
+print(100/2)
+print(100//2)
