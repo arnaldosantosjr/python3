b4e31e7d43d981d5c78965b7744da91d1a76a7c2
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-04-09 16:10:05 -0300

    Variaveis_e_Costantes
    diferenciando uma variável de uma constante em py.

---
+++ DioMe/Aula02/02Variaveis_Costantes.py
@@ -0,0 +1,21 @@
+#Posso declarar várias variáveis em penas uma linha.
+age, name = (23, 'Arnaldo')
+
+name = 'Antony' #Auterei o nome
+print(f'Meu nome é {name}, e eu tenho {age} anos.')
+
+
+
+limite_saque_diario = 1000
+limiteSaqueDiario =1000
+#Uma cosante nasce como as variáveis mas o valores são imutáveis 
+#Todos os caracateres ficam em maiúsculos
+DEBUG = True
+STATES = [
+    'SP',
+    'RJ',
+    'MG',
+
+]
+AMOUNT = 30.2
+print(STATES)
