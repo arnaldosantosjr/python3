3c8405a344ba20f096b63a8eea3a3310b73a4a06
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-04-18 17:00:25 -0300

    Update

--- cursoemvideo/exercícios/ex021.py
+++ cursoemvideo/exercícios/ex021.py
@@ -1,6 +1,17 @@
 #O programa irá abrir e reproduzir um áudio de arquivo mp3.
 # pip install playsound==1.2.2
 
-from playsound import playsound
-playsound("naturezamp3")
+'''import playsound
+print ("door.mp3")'''
 
+'''from playsound import playsound
+playsound('door.mp3')'''
+
+from playsound import playsound
+import os
+
+# Obtenha o caminho absoluto para o arquivo door.mp3
+caminho_absoluto = os.path.abspath('door.mp3')
+
+# Reproduzir o arquivo de áudio usando o caminho absoluto
+playsound(caminho_absoluto)
