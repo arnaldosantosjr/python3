00d9bb6b2ed0ea86cc22be56f25e0fc551540145
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2024-05-22 21:08:07 -0300

    Update ex022.py

--- cursoemvideo/exercícios/ex022.py
+++ cursoemvideo/exercícios/ex022.py
@@ -1,14 +1,20 @@
-nome= input('Informe o seu nome completo:')
+nome= str(input('Informe o seu nome completo:')).strip()#Strip() Serve para cortar as string para eliminar a contagem dos espaços
 
-print(f'Seu nome completo é {nome}.')
+print('Analisando seu nome...')
+print(f'Seu nome completo é:\n {nome}.')
 
 #Nome com todas as letras maiúsculas
-print(nome.upper())
+print(f'Seu nome em Músculo é: {nome.upper()}')
+
 #Todas as letras minúsculas
-print(nome.lower())
-#quantidade de letrsa (não os caractéres)
-numero_letras = len([letra for letra in nome if letra.isalpha()])
-print(numero_letras)
+print(f'Seu nome com todas as letras Minúsculas é: {nome.lower()}')
+
+#quantidade de letras (não os caractéres)
+print(f'O nimero de letras do seu nome é:{len(nome)-nome.count(' ')}')
 
 #quantidade de letras no primeiro nome
-numero_letras = len([letra for letra in nome if letra.isalpha()])
+'''print(f'Seu primeiro nome tem {nome.find(' ')} letras') #Essa é a primeira forma'''
+
+#quantidade de letras no primeiro nome (Segunda forma)
+separa = nome.split()#Faz a separação das palavras contidas na frase
+print(f'Seu primeiro nome é {separa[0]} e ele tem {len(separa[0])} letras')
